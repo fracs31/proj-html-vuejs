f2f38551ffd0e61aa8addb639b5e3dde71c501e9
fracs31 <pist.fracs95@gmail.com>
2023-03-13 13:52:57 +0100

    ADD header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <!-- JavaScript -->
 <script>
+import Header from "./components/Header.vue"; //importo l'header
+
+//Esporto
+export default {
+    //Componenti
+    components: {
+        Header, //header
+    }
+}
 </script>
 
 <!-- Template -->
 <template>
+    <!-- Header -->
+    <Header></Header>
 </template>
 
 <!-- CSS -->
---
+++ src/components/Header.vue
@@ -0,0 +1,125 @@
+<!-- JavaScript -->
+<script>
+</script>
+
+<!-- Template -->
+<template>
+    <!-- Header -->
+    <header class="main-header">
+        <!-- Container -->
+        <div class="container">
+            <!-- Navbar -->
+            <nav class="navbar">
+                <!-- Logo -->
+                <div class="logo">
+                    <!-- Immagine -->
+                    <img class="logo__img" src="../assets/img/logo-2.png" alt="Logo">
+                </div>
+                <!-- Lista -->
+                <ul class="list">
+                    <!-- Elemento della lista -->
+                    <li class="list-item active">
+                        Home
+                    </li>
+                    <!-- Elemento della lista -->
+                    <li class="list-item">
+                        About us
+                    </li>
+                    <!-- Elemento della lista -->
+                    <li class="list-item">
+                        Feature
+                    </li>
+                    <!-- Elemento della lista -->
+                    <li class="list-item">
+                        Testimonials
+                    </li>
+                    <!-- Elemento della lista -->
+                    <li class="list-item">
+                        Contact US
+                    </li>
+                </ul>
+                <!-- Azioni -->
+                <div class="actions">
+                    <!-- Ricerca -->
+                    <span class="search">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </span>
+                    <!-- Carrello -->
+                    <span class="cart">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </span>
+                </div>
+                <!-- Get started -->
+                <div class="get-started">
+                    <!-- Bottone -->
+                    <button class="btn">
+                        Get Started
+                    </button>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<!-- CSS -->
+<style scoped>
+    /* Header */
+    .main-header {
+        padding: 10px 0px;
+    }
+    /* Navbar */
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    /* Immagine */
+    .logo__img {
+        width: 45px;
+    }
+    /* Lista */
+    .list {
+        flex-basis: 65%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        gap: 30px;
+    }
+    /* Elemento della lista */
+    .list-item {
+        color: var(--first-black);
+        font-size: 16px;
+        cursor: pointer;
+    }
+    /* Active */
+    .list-item.active {
+        color: var(--blue);
+        position: relative;
+    }
+    /* Linea */
+    .list-item.active::after {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 2px;
+        background-color: var(--blue);
+        position: absolute;
+        left: 0px;
+        top: 30px;
+    }
+    /* Azioni */
+    .actions {
+        display: flex;
+        gap: 25px;
+    }
+    /* Bottone */
+    .btn {
+        color: white;
+        background-color: var(--blue);
+        border: none;
+        border-radius: 4px;
+        font-size: 16px;
+        padding: 10px 15px;
+        cursor: pointer;
+    }
+</style>
