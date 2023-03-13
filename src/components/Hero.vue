a902d3e4e95640ab7b3c52c45e0cce16bf3122d3
fracs31 <pist.fracs95@gmail.com>
2023-03-13 15:07:38 +0100

    ADD hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,78 @@
+<!-- JavaScript -->
+<script>
+</script>
+
+<!-- Template -->
+<template>
+    <!-- Hero -->
+    <section class="hero">
+        <!-- Box -->
+        <div class="box">
+            <!-- Descrizione della box -->
+            <div class="box__description">
+                <!-- Titolo -->
+                <h1 class="box__description--title">
+                    The biggest platform for your growth.
+                </h1>
+                <!-- Testo -->
+                <p class="box__description--text">
+                    There are many variations of passages of Lorem Ipsum available,
+                    but the majority have suffered alteration in some form
+                </p>
+                <!-- Bottone -->
+                <button class="box__description--btn btn">
+                    Read More
+                </button>
+            </div>
+            <!-- Immagine della box -->
+            <div class="box__image">
+                <!-- Immagine -->
+                <img class="box__image--img" src="../assets/img/508.jpeg" alt="Hero">
+            </div>
+        </div>
+    </section>
+</template>
+
+<!-- CSS -->
+<style scoped>
+    /* Hero */
+    .hero {
+        padding: 10px 150px;
+    }
+    /* Box */
+    .box {
+        background-color: var(--very-light-blue);
+        border-radius: 6px;
+        padding: 60px;
+        display: flex;
+        gap: 60px;
+    }
+    /* Titolo */
+    .box__description--title {
+        font-weight: normal;
+        font-size: 42px;
+        margin-bottom: 30px;
+    }
+    /* Testo */
+    .box__description--text {
+        font-size: 14px;
+        margin-bottom: 40px;
+    }
+    /* Bottone */
+    .btn {
+        color: white;
+        background-color: var(--blue);
+        border: none;
+        border-radius: 4px;
+        font-size: 16px;
+        padding: 10px 15px;
+        cursor: pointer;
+    }
+    /* Immagine usata */
+    .box__image--img {
+        width: 700px;
+        display: block;
+        margin-bottom: -160px;
+        margin-top: -40px;
+    }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,22 @@
 <!-- JavaScript -->
 <script>
+import Hero from "./Hero.vue"; //importo l'hero
+
+//Esporto
+export default {
+    //Componenti
+    components: {
+        Hero,
+    }
+}
 </script>
 
 <!-- Template -->
 <template>
     <!-- Main -->
     <main class="main-content">
-        
+        <!-- Hero -->
+        <Hero></Hero>
     </main>
 </template>
 
